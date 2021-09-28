4dc0b0b4f9a237e36c5de6858908c080da0f4179
Leshnevskyi <leshnevskyi@gmail.com>
2021-09-28 20:42:50 +0300

    feat: utility function for counting decimal places

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,4 +10,10 @@ function generateRandomNumbers(
   });
 }
 
-export {generateRandomNumbers};
+function countDecimalPlaces(number: number) {
+  if (Math.floor(number) === number) return 0;
+
+  return number.toString().split('.')[1].length || 0; 
+}
+
+export {generateRandomNumbers, countDecimalPlaces};

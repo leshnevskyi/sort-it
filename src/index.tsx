31337b08c13c077603dca22ab82d91e508c7b94d
Leshnevskyi <leshnevskyi@gmail.com>
2021-11-03 01:18:39 +0200

    refactor: move the app component to the renderer file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router, 
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
-import App from './components/App';
+import GlobalStyle from 'components/GlobalStyle';
+import {Bars} from 'components/bars';
+import {ContentWrapper, NavBar, StatusBar} from 'components/layout';
+import {SortingProvider} from 'context/sorting';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle/>
+      <Router>
+        <Route exact path='/'>
+          <Redirect to='/bubble-sort'/>
+        </Route>
+        <SortingProvider>
+          <ContentWrapper>
+            <NavBar/>
+            <Bars/>
+          </ContentWrapper>
+          <StatusBar/>
+        </SortingProvider>
+      </Router>
+    </>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
